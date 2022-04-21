e4cf4a8d75336600743ec44aafe8d92666897592
isyanavalda <100085573+isyanavalda@users.noreply.github.com>
2022-04-21 10:29:30 +0700

    Create variabel.php

---
+++ variabel.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Variabel </title>
+</head>
+<body>
+    <?php
+    $nilai_1 = 10; // Mendeklarasikan variabel $nilai_1
+    $nilai_2 = 3; // Mendeklarasikan variabel $nilai_2
+    $nilai_3 = 2 * $nilai_1 + 8 * $nilai_2; // Mendeklarasikan variabel $nilai_3
+    echo "nilai = "  , $nilai_3; // Menampilkan teks "nilai = 44"
+    echo "<br>"; // Ganti baris
+    $jumlah = $nilai_1 + $nilai_2; // Mendeklarasikan variabel $jumlah
+    echo "hasil dari $nilai_1 + $nilai_2 adalah: $jumlah"; // Menampilkan teks "hasil dari 10 + 3 adalah: 13"
+    echo "<br><br>"; // Ganti baris sebanyak 2
+    echo "\"Nama: Isyana Valda Choirila Vernanda\" <br>"; // Menampilkan teks "Nama: Isyana Valda Choirila Vernanda" kemudian ganti baris
+    echo "NPM: 20082010142"; // Menampilkan teks NIM: 20082010142
+    ?>
+</body>
+</html>
