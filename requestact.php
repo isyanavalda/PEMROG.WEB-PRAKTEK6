a583be5c0f9085713283c2591ca5ab1cd9533b3d
isyanavalda <100085573+isyanavalda@users.noreply.github.com>
2022-04-21 10:53:11 +0700

    Create requestact.php

---
+++ requestact.php
@@ -0,0 +1,7 @@
+<?php
+// Menampilkan output nilai variabel nama yang telah diinputkan pada form request.php
+echo "<center> Nama: ".$_REQUEST['nama']."</center><br>";
+
+// Menampilkan output nilai variabel email yang telah diinputkan pada form request.php
+echo "<center> Email: ".$_REQUEST['email']."</center><br>";
+?>
