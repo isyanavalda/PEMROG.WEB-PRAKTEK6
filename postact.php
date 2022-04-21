f163d02622cbd69640ae09056dc19fd7b3c65f7c
isyanavalda <100085573+isyanavalda@users.noreply.github.com>
2022-04-21 10:51:23 +0700

    Create postact.php

---
+++ postact.php
@@ -0,0 +1,7 @@
+<?php
+// Menampilkan output nilai variabel nama yang telah diinputkan pada form post.php dengan menggunakan metode method post
+echo "<center> Nama: ".$_POST['nama']."</center><br>";
+
+// Menampilkan output nilai variabel email yang telah diinputkan pada form post.php dengan menggunakan metode method post
+echo "<center> Email: ".$_POST['email']."</center><br>";
+?>
