02c9bd12c16cb372d3db682b9d86eda30cb73d39
isyanavalda <100085573+isyanavalda@users.noreply.github.com>
2022-04-21 10:52:11 +0700

    Create getact.php

---
+++ getact.php
@@ -0,0 +1,7 @@
+<?php
+// Menampilkan output nilai variabel nama yang telah diinputkan pada form get.php dengan menggunakan metode method post
+echo "<center> Nama: ".$_GET['nama']."</center><br>";
+
+// Menampilkan output nilai variabel email yang telah diinputkan pada form get.php dengan menggunakan metode method post
+echo "<center> Email: ".$_GET['email']."</center><br>";
+?>
