7d87a337c38787c6e572f09d3da357c3a1ca460b
isyanavalda <100085573+isyanavalda@users.noreply.github.com>
2022-04-21 10:52:46 +0700

    Create request.php

---
+++ request.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <form method="REQUEST" action="requestact.php">
+            <a href="http://localhost/tugas6/requestact.php?nama=Isyana+Valda+Choirila+Vernanda&email=isyanavaldaa%gmail.com&btnLogin=Login">
+            Link ke Cek </a>
+            <table width="400" align="center" cellpadding="2" cellspacing="2">
+                <tr>
+                    <td width="130"> Nama </td>
+                    <td><input type="text" name="nama"></td>
+                </tr>
+                <tr>
+                    <td width="130"> Email </td>
+                    <td><input type="text" name="email"></td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="submit" name="btnLogin" value="Login">
+                        <input type="reset" name="reset" value="Reset">
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
