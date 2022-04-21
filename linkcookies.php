900d5bbef26f8591f750866d3f4dfbde9e90228e
isyanavalda <100085573+isyanavalda@users.noreply.github.com>
2022-04-21 10:55:22 +0700

    Create linkcookies.php

---
+++ linkcookies.php
@@ -0,0 +1,8 @@
+<?php
+if (isset($variable_cookies)){ // Melakukan seleksi looping
+    echo 'Variable cookiesnya "$variable_cookies"_ nilainya adalah'.$variable_cookies; 
+    // Menampilkan teks 'Variable cookiesnya ....' jika nilai variabel cookiesnya diinputkan
+} else {
+    echo "Variable cookies belum diterapkan"; // Menampilkan teks "Variable cookies belum diterapkan"
+}
+?>
