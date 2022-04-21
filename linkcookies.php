f8d8a2eeb266039ac40c2ea521e4b6f029f5fd23
isyanavalda <100085573+isyanavalda@users.noreply.github.com>
2022-04-21 10:56:39 +0700

    Create linkcookies.php

---
+++ linkcookies.php
@@ -0,0 +1,4 @@
+<?php
+setcookie("variable_cookies","ini adalah variable cookies", time()+60); // Menerapkan pengaturan cookies
+echo "<a href=cekcookies.php> Cek Cookies </a>" // Tempat penyimpanan nilai untuk menuju form cekcookies.php
+?>
