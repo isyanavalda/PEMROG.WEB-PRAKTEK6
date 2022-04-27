f0d9fb898d1637839736538ddb714337e74d769c
isyanavalda <100085573+isyanavalda@users.noreply.github.com>
2022-04-27 13:18:26 +0700

    Create soal3.php

---
+++ soal3.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Tugas 3 </title>
+    </head>
+    <body>
+        <center><h1> FORM BIODATA </h1></center>
+        <form method="POST" action="soal3act.php">
+            <table width="400" align="center" cellpadding="2" cellspacing="2">
+                <tr>
+                    <td width="130"> Nama Lengkap </td>
+                    <td><input type="text" name="namalengkap"></td>
+                </tr>
+                <tr>
+                    <td width="130"> NPM </td>
+                    <td><input type="text" name="npm"></td>
+                </tr>
+                <tr>
+                    <td width="130"> Tempat, Tanggal Lahir </td>
+                    <td><input type="text" name="ttl"></td>
+                </tr>
+                <tr>
+                    <td width="130"> Alamat </td>
+                    <td><input type="text" name="alamat"></td>
+                </tr>
+                <tr>
+                    <td width="130"> Hobi </td>
+                    <td><input type="text" name="hobi"></td>
+                </tr>
+                <tr>
+                    <td width="130"> Email </td>
+                    <td><input type="text" name="email"></td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="submit" name="btnLogin" value="Login">
+                        <input type="reset" name="reset" value="Reset">
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
